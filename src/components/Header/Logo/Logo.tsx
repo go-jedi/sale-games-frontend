3eab3a04c9849ae0fcb917b8bb47f2e81f7fe52a
rob-bender <rob_bender@inbox.lv>
2023-11-02 19:35:37 +0500

    add click logo route home

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import LogoImg from '@/assets/img/logo.svg';
 
@@ -8,8 +9,10 @@ import styles from '@/components/Header/Logo/Logo.module.scss';
 const Logo: React.FC = () => {
   return (
     <div className={styles.logo}>
-      <Image className={styles.logo_img} src={LogoImg} priority={true} alt="logo" />
-      <div className={styles.logo_title}>Games Rent</div>
+      <Link className={styles.link} href={'/'}>
+        <Image className={styles.logo_img} src={LogoImg} priority={true} alt="logo" />
+        <div className={styles.logo_title}>Games Rent</div>
+      </Link>
     </div>
   );
 };
