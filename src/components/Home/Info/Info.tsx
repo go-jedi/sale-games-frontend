01f9778b11c6453d2550029718f3b90be6a18383
rob-bender <rob_bender@inbox.lv>
2023-11-08 18:06:50 +0500

    add info blocks in home

--- src/components/Home/Info/Info.tsx
+++ src/components/Home/Info/Info.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
 
+import { WifiOutlined, ClockCircleOutlined, TeamOutlined } from '@ant-design/icons';
+
 import styles from '@/components/Home/Info/Info.module.scss';
+
+type InformationsType = {
+  id: number;
+  title: string;
+  text: string;
+  icon: any;
+};
+
+const informations: InformationsType[] = [
+  {
+    id: 1,
+    title: 'Поддержка',
+    text: 'Круглосуточная обратная связь от наших специалистов.',
+    icon: <WifiOutlined className={styles.icon} />,
+  },
+  {
+    id: 2,
+    title: 'Магазин',
+    text: 'Если вы хотите сыграть в любимую игру, давайте сделаем это. С Youplay это так просто.    ',
+    icon: <ClockCircleOutlined className={styles.icon} />,
+  },
+  {
+    id: 3,
+    title: 'Аренда',
+    text: 'Если вы хотите зарабатывать на своём аккаунте, можете сдать его в аренду.',
+    icon: <TeamOutlined className={styles.icon} />,
+  },
+];
 
 const Info: React.FC = () => {
   return (
     <div className={styles.info}>
-      <div>Info</div>
+      {informations.map((e: InformationsType) => {
+        return (
+          <div key={e.id} className={styles.block}>
+            <div className={styles.img}>{e.icon}</div>
+            <div className={styles.title}>{e.title}</div>
+            <div className={styles.text}>{e.text}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
